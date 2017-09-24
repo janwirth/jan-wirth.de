91f4a3453925d69487a9eab4e8d149312b84fde3
Jan Wirth <jottenwe@gmail.com>
2017-09-24 12:42:23 +0200

    Remove background animation

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,4 @@
 <template lang='pug'>
   #app
-    canvas#canvas
     router-view
 </template>
-
-<script>
-import background from './components/background'
-background
-export default {
-  data () {
-    if (global.window && (global.window.innerWidth > 1000)) {
-      const c = window.document.querySelector('#canvas')
-      console.log(c)
-      background(c)
-    }
-    /* eslint-disable no-undef */
-    /* eslint-enable no-undef */
-    return {}
-  }
-}
-</script>
-
-<style lang='stylus'>
-#canvas
-  position: fixed
-  top: 0
-  left: 0
-  min-height: 100vh
-  min-width: 100vw
-  @media print
-    display: none
-
-</style>
